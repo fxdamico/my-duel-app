d6732cefea5b6f6e907823f4fca7efb7af74d5c7
user115274 <user115274@74-80-228-163.ic2net.net>
2016-01-10 11:53:16 -0800

    Initial Commit

---
+++ my-duel-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-duel-app
+//
+//  Created by user115274 on 1/10/16.
+//  Copyright © 2016 Frank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
